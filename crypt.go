225c5a0722a15ef3c03cee4ea5f6f4344bdffa99
Junkes887 <henriquejunkes887@gmail.com>
2021-04-17 22:30:58 -0300

    add cryptInt

--- crypt.go
+++ crypt.go
@@ -3,7 +3,9 @@ package artifacts
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func Encrypt(valor string) []byte {
@@ -12,11 +14,29 @@ func Encrypt(valor string) []byte {
 	plaintext := []byte(valor)
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err.Error())
+		fmt.Println(err)
 	}
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		fmt.Println(err)
+	}
+	nonce := make([]byte, aesgcm.NonceSize())
+
+	ciphertext := aesgcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext
+}
+
+func EncryptInt(valor int) []byte {
+	CRYPT_KEY := os.Getenv("CRYPT_KEY")
+	key := []byte(CRYPT_KEY)
+	plaintext := []byte(strconv.Itoa(valor))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println(err)
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		fmt.Println(err)
 	}
 	nonce := make([]byte, aesgcm.NonceSize())
 
@@ -32,4 +52,18 @@ func Decrypt(ciphertext []byte) string {
 	plaintext, _ := aesgcm.Open(nil, ciphertext[:aesgcm.NonceSize()], ciphertext[aesgcm.NonceSize():], nil)
 
 	return string(plaintext)
+}
+
+func DecryptInt(ciphertext []byte) int {
+	CRYPT_KEY := os.Getenv("CRYPT_KEY")
+	key := []byte(CRYPT_KEY)
+	block, _ := aes.NewCipher(key)
+	aesgcm, _ := cipher.NewGCM(block)
+	plaintext, _ := aesgcm.Open(nil, ciphertext[:aesgcm.NonceSize()], ciphertext[aesgcm.NonceSize():], nil)
+
+	text, err := strconv.Atoi(string(plaintext))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return text
 }
